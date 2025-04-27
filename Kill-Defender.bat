599803aa2eb719bc094e6b8754a3a3a582b148b5
ze0r <43227253+ze0r@users.noreply.github.com>
2025-04-27 16:15:43 +0800

    Update Kill-Defender.bat

--- Kill-Defender.bat
+++ Kill-Defender.bat
@@ -1,5 +1,12 @@
 echo off
+bcdedit.exe /enum {current} | findstr "safeboot" >nul && goto STARTDELETE || goto SETSAFEMODE
 
+:SETSAFEMODE
+bcdedit.exe /set {current} safeboot minimal >nul
+echo Setting safeboot mode has been done.
+goto REBOOT
+
+:STARTDELETE
 :: Delete System32 files
 takeown /F "C:\Windows\System32\SecurityHealth*" /A
 icacls "C:\Windows\System32\SecurityHealth*" /grant Administrators:(F) 
@@ -23,7 +30,6 @@ icacls "C:\Program Files\Windows Defender Advanced Threat Protection" /grant Adm
 rmdir /S /Q "C:\Program Files\Windows Defender Advanced Threat Protection"
 
 :: Delete service in registration
-
 sc Config TrustedInstaller binPath= "cmd.exe /C reg delete HKLM\SYSTEM\CurrentControlSet\Services\SecurityHealthService /f"
 sc start TrustedInstaller
 sc start TrustedInstaller
@@ -35,3 +41,9 @@ sc Config TrustedInstaller binPath= "%systemroot%\servicing\TrustedInstaller.exe
 :: Add regedit key to disable VBS & HVCI
 REG ADD HKLM\SYSTEM\CurrentControlSet\Control\DeviceGuard /v EnableVirtualizationBasedSecurity /t REG_DWORD /d 0 /f
 REG ADD HKLM\SYSTEM\CurrentControlSet\Control\DeviceGuard\Scenarios\HypervisorEnforcedCodeIntegrity /v Enabled /t REG_DWORD /d 0 /f
+
+bcdedit.exe /deletevalue {current} safeboot
+
+:REBOOT
+set /p rbt="Ready to reboot? (Y/n): "
+if NOT "%rbt%"=="n" shutdown /r /t 1
